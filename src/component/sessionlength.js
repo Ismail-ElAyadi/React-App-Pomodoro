dbdfce81ba4a21e185450d1b7f910a5b356b49ea
ismail <ismail.elydi@gmail.com>
2019-11-28 10:21:56 +0100

    EsLint clear

--- src/component/sessionlength.js
+++ src/component/sessionlength.js
@@ -1,27 +1,25 @@
-import React from 'react';
-function SessionLength(props){
-
-    function increaseSession(){
-if (props.sessionLength===60){
-    return;}
-    props.increaseSession();
-
+import React from "react";
+function SessionLength(props) {
+    function increaseSession() {
+        if (props.sessionLength === 60) {
+            return;
+        }
+        props.increaseSession();
     }
 
-
-    function decreaseSession(){
-        if (props.sessionLength===1){
+    function decreaseSession() {
+        if (props.sessionLength === 1) {
             return;
-
         }
-        props.decreaseSession(); }
-    return(
+        props.decreaseSession();
+    }
+    return (
         <section>
             <h4>Session Length</h4>
-            <button onClick = {decreaseSession}>down</button>
+            <button onClick={decreaseSession}>down</button>
             <p>{props.sessionLength}</p>
-            <button onClick = {increaseSession} >up</button>
+            <button onClick={increaseSession}>up</button>
         </section>
-    )
+    );
 }
-export default SessionLength
+export default SessionLength;
--- src/component/timer.js
+++ src/component/timer.js
@@ -6,35 +6,28 @@ class Timer extends React.Component {
             isSession: true,
             timerSecond: 0,
         };
-        this.play = this.play.bind(this)
+        this.play = this.play.bind(this);
     }
-    play(){
-
-    }
+    play() {}
     render() {
         return (
             <section>
-                    <h4>
-                        {this.state.isSession === true ? "Session" : "break"}
-                    </h4>
-                    <span>{this.props.timerMinute}</span>
-                    <span>:</span>
-                    <span>
-                        {this.state.timerSecond === 0
-                            ? "00"
-                            : this.state.timerSecond < 10
-                            ? `0${this.state.timerSecond}`
-                            : this.state.timerSecond}{" "}
-                    </span>
-                    <section>
-                <button onClick={this.play} >Play</button>
-                <button onClick={this.stop} >stop</button>
-                <button onClick={this.reset} >Refresh</button>
-
+                <h4>{this.state.isSession === true ? "Session" : "break"}</h4>
+                <span>{this.props.timerMinute}</span>
+                <span>:</span>
+                <span>
+                    {this.state.timerSecond === 0
+                        ? "00"
+                        : this.state.timerSecond < 10
+                        ? `0${this.state.timerSecond}`
+                        : this.state.timerSecond}{" "}
+                </span>
+                <section>
+                    <button onClick={this.play}>Play</button>
+                    <button onClick={this.stop}>stop</button>
+                    <button onClick={this.reset}>Refresh</button>
+                </section>
             </section>
-            </section>
-
-          
         );
     }
 }
